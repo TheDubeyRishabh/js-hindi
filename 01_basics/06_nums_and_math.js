e8f65985803be53975de1cf5967524fa424e6d37
TheDubeyRishabh <therishabhdube@gmail.com>
2025-02-24 06:42:02 +0000

    Math_and_Run

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,20 @@
+const score=400
+//console.log(score);
+
+const balance =new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString());  // Convert to string
+//console.log(balance.toFixed(2));  // Convert number to string with 2 decimal places
+
+
+const otherNumber=123.8968
+//console.log(otherNumber.toPrecision(4));
+//**************MATH************* */
+//console.log(Math);
+console.log(Math.random());
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
