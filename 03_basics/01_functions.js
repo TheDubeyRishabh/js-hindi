426c116a8adbbd0ef6f3da4f1993f5a1cfffc6ea
TheDubeyRishabh <therishabhdube@gmail.com>
2025-03-01 19:15:30 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,28 @@
+function sayMyName() { 
+    console.log("R");
+    console.log("I");
+    console.log("S");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("B");
+    console.log("H");
+}
+
+// Call the function
+// sayMyName(); 
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1 + number2);  // Fixed console.log()
+// }
+
+function addTwoNumbers(number1, number2) {
+    // let result= number1+number2
+    // return result;
+    return  number1+number2;
+      
+}
+
+// Call the function
+ const result =addTwoNumbers(3, 4);
+ console.log(result);
