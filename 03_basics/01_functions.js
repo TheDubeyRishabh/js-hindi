fda917fc9ca2e530ef2ea956825e2e1e25d4a0f4
TheDubeyRishabh <therishabhdube@gmail.com>
2025-03-03 06:56:12 +0000

    fun

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,28 +1,39 @@
-function sayMyName() { 
-    console.log("R");
-    console.log("I");
-    console.log("S");
-    console.log("S");
-    console.log("H");
-    console.log("A");
-    console.log("B");
-    console.log("H");
+function sayMyName() {
+    const name = "RISHABH";
+    for (const letter of name) {
+        console.log(letter);
+    }
 }
 
 // Call the function
-// sayMyName(); 
-
-// function addTwoNumbers(number1, number2) {
-//     console.log(number1 + number2);  // Fixed console.log()
-// }
+// sayMyName();
 
 function addTwoNumbers(number1, number2) {
-    // let result= number1+number2
-    // return result;
-    return  number1+number2;
-      
+    return number1 + number2;
 }
 
 // Call the function
- const result =addTwoNumbers(3, 4);
- console.log(result);
+// const result = addTwoNumbers(3, 4);
+// console.log(result);
+
+function loginUserMessage(username) {
+    if (!username) {
+        return "User not found!";
+    }
+    return `${username.trim()} just logged in`;
+}
+
+//console.log(loginUserMessage(" rishabh "));
+function calculateCarPrice(...num1){// ... rest operator  
+    return num1
+}
+//console.log(calculateCarPrice(200,400,500));
+
+const user={
+    username:"rishabh",
+    price:199
+}
+function handleObject(anyobjest){
+    console.log(`Username is ${anyobjest.username} and price is ${anyobjest.price} `);
+}
+handleObject(user)
